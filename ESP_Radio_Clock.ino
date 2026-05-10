c024a2e1fad1af69dacd2795ccaf5dd38b329609
PetervanderBurgt <jeper27@gmail.com>
2026-05-10 19:58:47 +0200

    Added some simple debugging and time retrieval syncing fix

--- ESP_Radio_Clock.ino
+++ ESP_Radio_Clock.ino
@@ -10,6 +10,8 @@ NtpServer timeServer;
 TM1637Wrapper Tm1637;
 CSE_MillisTimer secondTimer(1000);  // Create a timer with a time period of 500 ms.
 
+uint32_t time_to_resync = 030000;  // Formatted in HHmmss
+
 tm current_time;  // Structure to hold the current time.
 
 // the setup function runs once when you press reset or power the board
@@ -29,6 +31,10 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
+  if (current_time.tm_hour * 10000 + current_time.tm_min * 100 + current_time.tm_sec * 1 == time_to_resync) {
+    current_time = timeServer.get_time();
+  }
+
   if (secondTimer.isElapsed()) {
     secondTimer.start();
     Tm1637.set_time(current_time.tm_hour, current_time.tm_min, current_time.tm_sec);
@@ -37,5 +43,6 @@ void loop() {
     time_t rawTime = mktime(&current_time);
     rawTime++;
     localtime_r(&rawTime, &current_time);
+    Serial.println(current_time.tm_hour * 10000 + current_time.tm_min * 100 + current_time.tm_sec * 1);
   }
 }
